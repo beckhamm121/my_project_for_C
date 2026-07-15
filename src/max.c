cc8f76f424f4d1e8c60c2778232a6d9c622526e8
Ibragim <ibragim@example.com>
2026-07-15 12:28:01 +0700

    code for quest4

---
+++ src/max.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int max(int x, int y) {
+    int result = x;
+    if (x < y) {
+        result = y;
+    }
+    return result;
+}
+
+int main(void) {
+    int a, b;
+    int ch;
+    if (scanf("%d%d", &a, &b) != 2) {
+        printf("n/a");
+        return 1;
+    }
+    
+    ch = getchar();
+
+    if (ch == '\n' || ch == EOF) {
+        if (a == b) {
+            printf("%d", a);
+            return 0;
+        }
+        printf("%d", max(a, b));
+        return 0;
+    }
+
+    printf("n/a");
+    return 1;
+
+}
